e31e10175aa8fc6589a85c52aa5bc5b55a91f0cc
Matt White <mattwhite614@gmail.com>
2015-07-28 20:18:51 -0500

    needs styling

--- myJs.js
+++ myJs.js
@@ -1,17 +1,30 @@
-$(function(){
+//get value from form 
+$("#submit").submit(function(event){
+	event.preventDefault();
+var submittal = document.getElementById('submittal').value;
+//encode submital
+submittal = encodeURI(submittal)
 
+// Submit request and return results from API
 $.ajax({
-url: 'https://api.github.com/search/repositories?q=php',
+url: 'https://api.github.com/search/repositories?q=' + submittal,
 method: 'GET',
 success: function(data){
-	var giveIt = data.items;
+	var subArray = data.items;
 
- 
+	for(i = 0; i < subArray.length; i ++){
+  	var RepoName = subArray[i].full_name;
+  	//refactor if RepoOwner is undefined omit entry
+  	var RepoOwner = subArray[i].login
+  	var RepoUrl = subArray[i].html_url;
+  	var RepoDescript = subArray[i].description;
+  	
+  	//Append DOM
+  	document.write(RepoName + ' by ' + RepoOwner + '<br>')
+  	document.write(RepoUrl + '<br>')
+  	document.write(RepoDescript + '<br>' + '<hr />')
+}; 
 
- for(i = 0; i < giveIt.length; i ++){
-  var url = giveIt[i].git_url
-  document.write(url + '</br>')
-} 
 	}
 	
 
